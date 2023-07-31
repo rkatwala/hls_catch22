fd339e9e1a888acd87536f82dbd7544855208287
Rararahul <58281823+rkatwala@users.noreply.github.com>
2023-07-31 14:01:56 -0700

    Update host.cpp
    Finished host.cpp for histFeature

--- src/host.cpp
+++ src/host.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cmath>
 #include <cstdlib>
+#include "stats.h"
+#include <float.h>
 
 /* Orginal code from C22 https://github.com/DynamicsAndNeuralSystems/catch22/tree/main */
 // double DN_Mean(const double a[], const int size)
@@ -16,14 +18,137 @@
 // }
 
 /* Modified */
-double DN_Mean(data_t* a)
+double mean(data_t a[DATA_SIZE], const int size)
 {
     double m = 0.0;
-    for (int i = 0; i < DATA_SIZE; i++) {
+    for (int i = 0; i < size; i++) {
         m += a[i];
     }
-    m /= DATA_SIZE;
+    m /= size;
     return m;
+}
+
+double stddev(data_t a[DATA_SIZE], const int size) {
+    double m = mean(a, size);
+    double sd = 0.0;
+    for (int i = 0; i < size; i++) {
+        sd += pow(a[i] - m, 2);
+    }
+    sd = sqrt(sd / (size - 1));
+    return sd;
+}
+
+int histcounts(data_t y[DATA_SIZE], const int size, int nBins, int* binCounts, double* binEdges) 
+{
+
+     int i = 0;
+    // check min and max of input array
+    double minVal = DBL_MAX, maxVal=-DBL_MAX;
+    for(int i = 0; i < size; i++)
+    {
+        // printf("histcountInput %i: %1.3f\n", i, y[i]);
+        
+        if (y[i] < minVal)
+        {
+            minVal = y[i];
+        }
+        if (y[i] > maxVal)
+        {
+            maxVal = y[i];
+        }
+    }
+    
+    // if no number of bins given, choose spaces automatically
+    if (nBins <= 0){
+        nBins = ceil((maxVal-minVal)/(3.5*stddev(y, size)/pow(size, 1/3.)));
+    }
+
+    // and derive bin width from it
+    double binStep = (maxVal - minVal)/nBins;
+    
+    
+    for(i = 0; i < nBins; i++) {
+        binCounts[i] = 0;
+    }
+
+    for(i = 0; i < size; i++) {
+        int binInd = (y[i]-minVal)/binStep;
+        if(binInd < 0)
+            binInd = 0;
+        if(binInd >= nBins)
+            binInd = nBins-1;
+        binCounts[binInd] += 1;
+    }
+
+    for(i = 0; i < nBins+1; i++) {
+        binEdges[i] = i * binStep + minVal;
+    }
+
+    /*
+    // debug
+    for(i=0;i<nBins;i++)
+    {
+        printf("%i: count %i, edge %1.3f\n", i, binCounts[i], binEdges[i]);
+    }
+    */
+
+    return nBins;
+}
+
+data_t DN_HistogramMode_5(data_t y[DATA_SIZE])
+{
+    const int size = DATA_SIZE;
+    // NaN check
+    for(int i = 0; i < size; i++)
+    {
+        if(isnan(y[i]))
+        {
+            return NAN;
+        }
+    }
+    
+    //const int nBins = 5;
+    
+    int histCounts[5];
+    double binEdges[6];
+    
+    histcounts(y, size, 5, histCounts, binEdges);
+    
+    /*
+    for(int i = 0; i < nBins; i++){
+        printf("histCounts[%i] = %i\n", i, histCounts[i]);
+    }
+    for(int i = 0; i < nBins+1; i++){
+        printf("binEdges[%i] = %1.3f\n", i, binEdges[i]);
+    }
+     */
+    
+    double maxCount = 0;
+    int numMaxs = 1;
+    double out = 0;;
+    for(int i = 0; i < 5; i++)
+    {
+        // printf("binInd=%i, binCount=%i, binEdge=%1.3f \n", i, histCounts[i], binEdges[i]);
+        
+        if (histCounts[i] > maxCount)
+        {
+            maxCount = histCounts[i];
+            numMaxs = 1;
+            out = (binEdges[i] + binEdges[i+1])*0.5;
+        }
+        else if (histCounts[i] == maxCount){
+            
+            numMaxs += 1;
+            out += (binEdges[i] + binEdges[i+1])*0.5;
+        }
+    }
+    out = out/numMaxs;
+    
+    // arrays created dynamically in function histcounts
+    // free(histCounts);
+    // free(binEdges);
+    
+    return out;
 }
 
 int main(int argc, char** argv) {
@@ -92,7 +217,7 @@ int main(int argc, char** argv) {
 
     /*====================================================SW VERIFICATION===============================================================*/
     for (int j = 0; j < TOTAL_DATA_SIZE - DATA_SIZE; j++) {
-        output_sw[j] = DN_Mean(input_sw + j);
+        output_sw[j] = DN_HistogramMode_5(input_sw + j);
     }
     /*====================================================Setting up kernel I/O===============================================================*/
 
@@ -162,5 +287,4 @@ int main(int argc, char** argv) {
 
     return (match ? EXIT_SUCCESS : EXIT_FAILURE);
 }
-
 
