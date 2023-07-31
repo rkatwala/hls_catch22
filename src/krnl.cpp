c92a09d2e1a69cdb971f897add391b56cdb53903
Rararahul <58281823+rkatwala@users.noreply.github.com>
2023-07-30 21:43:57 -0700

    Update krnl.cpp
    Data type errors and histcount function "missing" errors

--- src/krnl.cpp
+++ src/krnl.cpp
@@ -1,4 +1,6 @@
 #include "constants.h"
+#include "stats.h"
+#include <float.h>
 /*
 
     From Catch 22 GitHub: https://github.com/DynamicsAndNeuralSystems/catch22/tree/main
@@ -8,17 +10,138 @@
     Modified to work with HLS
 
 */
-data_t DN_Mean(data_t a[DATA_SIZE])
+
+double stddev(const double a[], const int size)
 {
-    #pragma HLS INLINE
-    double m = 0.0;
-    for (int i = 0; i < DATA_SIZE; i++) {
-        #pragma HLS PIPELINE
-        m += a[i];
+    double m = mean(a, size);
+    double sd = 0.0;
+    for (int i = 0; i < size; i++) {
+        sd += pow(a[i] - m, 2);
     }
-    m /= DATA_SIZE;
-    return m;
+    sd = sqrt(sd / (size - 1));
+    return sd;
 }
+
+int histcounts(const double y[], const int size, int nBins, int ** binCounts, double ** binEdges)
+{
+
+    int i = 0;
+    // check min and max of input array
+    double minVal = DBL_MAX, maxVal=-DBL_MAX;
+    for(int i = 0; i < size; i++)
+    {
+        // printf("histcountInput %i: %1.3f\n", i, y[i]);
+        
+        if (y[i] < minVal)
+        {
+            minVal = y[i];
+        }
+        if (y[i] > maxVal)
+        {
+            maxVal = y[i];
+        }
+    }
+    
+    // if no number of bins given, choose spaces automatically
+    if (nBins <= 0){
+        nBins = ceil((maxVal-minVal)/(3.5*stddev(y, size)/pow(size, 1/3.)));
+    }
+    
+    // and derive bin width from it
+    double binStep = (maxVal - minVal)/nBins;
+    
+    // variable to store counted occurances in
+    *binCounts = malloc(nBins * sizeof(int));
+    for(i = 0; i < nBins; i++)
+    {
+        (*binCounts)[i] = 0;
+    }
+    
+    for(i = 0; i < size; i++)
+    {
+        
+        int binInd = (y[i]-minVal)/binStep;
+        if(binInd < 0)
+            binInd = 0;
+        if(binInd >= nBins)
+            binInd = nBins-1;
+        (*binCounts)[binInd] += 1;
+        
+    }
+    
+    *binEdges = malloc((nBins+1) * sizeof(double));
+    for(i = 0; i < nBins+1; i++)
+    {
+        (*binEdges)[i] = i * binStep + minVal;
+    }
+   
+    /*
+    // debug
+    for(i=0;i<nBins;i++)
+    {
+        printf("%i: count %i, edge %1.3f\n", i, binCounts[i], binEdges[i]);
+    }
+    */
+    
+    return nBins;
+    
+}
+data_t DN_HistogramMode_5(data_t y[DATA_SIZE])
+{
+    int size = DATA_SIZE;
+    // NaN check
+    for(int i = 0; i < size; i++)
+    {
+        if(isnan(y[i]))
+        {
+            return NAN;
+        }
+    }
+    
+    //const int nBins = 5;
+    
+    int * histCounts;
+    double * binEdges;
+    
+    histcounts(y, size, 5, &histCounts, &binEdges);
+    
+    /*
+    for(int i = 0; i < nBins; i++){
+        printf("histCounts[%i] = %i\n", i, histCounts[i]);
+    }
+    for(int i = 0; i < nBins+1; i++){
+        printf("binEdges[%i] = %1.3f\n", i, binEdges[i]);
+    }
+     */
+    
+    double maxCount = 0;
+    int numMaxs = 1;
+    double out = 0;;
+    for(int i = 0; i < 5; i++)
+    {
+        // printf("binInd=%i, binCount=%i, binEdge=%1.3f \n", i, histCounts[i], binEdges[i]);
+        
+        if (histCounts[i] > maxCount)
+        {
+            maxCount = histCounts[i];
+            numMaxs = 1;
+            out = (binEdges[i] + binEdges[i+1])*0.5;
+        }
+        else if (histCounts[i] == maxCount){
+            
+            numMaxs += 1;
+            out += (binEdges[i] + binEdges[i+1])*0.5;
+        }
+    }
+    out = out/numMaxs;
+    
+    // arrays created dynamically in function histcounts
+    free(histCounts);
+    free(binEdges);
+    
+    return out;
+}
+
 
 extern "C" void krnl(hls::stream<data_t> &input, hls::stream<data_t> &output) {
     #pragma HLS INTERFACE mode=axis port=&input
@@ -35,10 +158,8 @@ extern "C" void krnl(hls::stream<data_t> &input, hls::stream<data_t> &output) {
     input >> window[DATA_SIZE-1];
     
     /* Feature Extraction */
-    result = DN_Mean(window);
+    result = DN_HistogramMode_5(window);
 
     /* Pushing to FIFO Stream */
     output << result;
 }
-
-
