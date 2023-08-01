0660296658b0884e94a575c7504b7262dccb86a7
nmath018 <91509805+nmath018@users.noreply.github.com>
2023-07-31 21:49:50 -0700

    Update krnl.cpp
    Krnl.cpp for the mean feature

--- src/krnl.cpp
+++ src/krnl.cpp
@@ -1,4 +1,5 @@
 #include "constants.h"
+#include "stats.h"
 /*
 
     From Catch 22 GitHub: https://github.com/DynamicsAndNeuralSystems/catch22/tree/main
@@ -7,7 +8,7 @@
 
     Modified to work with HLS
 
-*/
+
 data_t DN_Mean(data_t a[DATA_SIZE])
 {
     #pragma HLS INLINE
@@ -19,6 +20,62 @@ data_t DN_Mean(data_t a[DATA_SIZE])
     m /= DATA_SIZE;
     return m;
 }
+*/
+double mean(const double a[], const int size)
+{
+    double m = 0.0;
+    for (int i = 0; i < size; i++) {
+        m += a[i];
+    }
+    m /= size;
+    return m;
+}
+double stddev(const double a[], const int size)
+{
+    double m = mean(a, size);
+    double sd = 0.0;
+    for (int i = 0; i < size; i++) {
+        sd += pow(a[i] - m, 2);
+    }
+    sd = sqrt(sd / (size - 1));
+    return sd;
+}
+
+data_t FC_LocalSimple_mean3_stderr(data_t y[DATA_SIZE])
+{
+    
+        // NaN check
+    int train_length = 3;
+    int size = DATA_SIZE;   
+    for(int i = 0; i < size; i++)
+    {
+        if(isnan(y[i]))
+        {
+            return NAN;
+        }
+    }
+    
+    double* res = new double[size - train_length];
+    
+    for (int i = 0; i < size - train_length; i++)
+    {
+        double yest = 0;
+        for (int j = 0; j < train_length; j++)
+        {
+            yest += y[i+j];
+            
+        }
+        yest /= train_length;
+        
+        res[i] = y[i+train_length] - yest;
+    }
+    
+    double output = stddev(res, size - train_length);
+    
+    //free(res);
+    return output;
+}
+
 
 extern "C" void krnl(hls::stream<data_t> &input, hls::stream<data_t> &output) {
     #pragma HLS INTERFACE mode=axis port=&input
@@ -35,7 +92,7 @@ extern "C" void krnl(hls::stream<data_t> &input, hls::stream<data_t> &output) {
     input >> window[DATA_SIZE-1];
     
     /* Feature Extraction */
-    result = DN_Mean(window);
+    result = FC_LocalSimple_mean3_stderr(window);
 
     /* Pushing to FIFO Stream */
     output << result;
